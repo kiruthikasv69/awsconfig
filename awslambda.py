326fbe9d9ee99393ff9357caf580e8b4d0606942
Kiruthika S <105112242+kiruthikasv69@users.noreply.github.com>
2025-06-16 22:24:59 +0530

    Update awslambda.py

--- awslambda.py
+++ awslambda.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+#Used to parse JSON-formatted strings (because AWS Config sends data in JSON format).
 
 def lambda_handler(event, context):
 
@@ -9,10 +10,13 @@ def lambda_handler(event, context):
     # Assume compliant by default
     compliance_status = "COMPLIANT"  
     
-    # Extract the configuration item from the invokingEvent
+    # Extract the configuration item from the invokingEvent  
+    #This is the event payload passed by AWS Config when it triggers your Lambda function.
+
     config = json.loads(event['invokingEvent'])
     
     configuration_item = config["configurationItem"]
+#Extracts the configuration item, which has metadata about the EC2 instance being evaluated (like instance ID, current settings).
     
     # Extract the instanceId
     instance_id = configuration_item['configuration']['instanceId']
@@ -20,7 +24,7 @@ def lambda_handler(event, context):
     # Get complete Instance details
     instance = ec2_client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
     
-    # Check if the specific EC2 instance has Cloud Trail logging enabled.
+    # Check if the specific EC2 instance has Cloud watch enabled.
     
     if not instance['Monitoring']['State'] == "enabled":
         compliance_status = "NON_COMPLIANT"
@@ -32,12 +36,26 @@ def lambda_handler(event, context):
         'Annotation': 'Detailed monitoring is not enabled.',
         'OrderingTimestamp': config['notificationCreationTime']
     }
+
+#This creates a dictionary (evaluation) that reports:
+#What resource was checked (EC2)
+
+#Which instance (instance_id)
+
+#Whether it’s compliant or not
+
+#Message annotation
+
+#When this check was triggered
     
     config_client = boto3.client('config')
+#Creates a client to interact with AWS Config (to send back results of evaluation).
     
     response = config_client.put_evaluations(
         Evaluations=[evaluation],
-        ResultToken=event['resultToken']
+        ResultToken=event['resultToken'] #It is a unique token provided by AWS Config during the evaluation trigger.
+
+#It is required when reporting the result back to AWS Config using put_evaluations().
     )  
     
     return response
